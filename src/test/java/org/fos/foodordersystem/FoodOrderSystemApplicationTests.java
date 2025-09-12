06b8637351e8addc7d1d0a61ccdbc9f55ed7bdd2
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-09-12 21:52:56 +0700

    feat: setting springboot

---
+++ src/test/java/org/fos/foodordersystem/FoodOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fos.foodordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
